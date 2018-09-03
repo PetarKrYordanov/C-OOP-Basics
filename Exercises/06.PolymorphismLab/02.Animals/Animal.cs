9dd7690030c29c815b7d0fc794a9125ad4a2337c
Petar Yordanov <pesho_kr@abv.bg>
2018-09-03 16:46:01 +0300

    Add Exercises/06.PolymorphismLab/

---
+++ Exercises/06.PolymorphismLab/02.Animals/Animal.cs
@@ -0,0 +1,17 @@
+﻿public class Animal
+{
+ public string Name { get;protected set; }
+
+    public string FavouriteFood { get;protected set; }
+
+    protected Animal (string name, string favouriteFood)
+    {
+        this.Name = name;
+        this.FavouriteFood = favouriteFood;
+    }
+
+    public virtual string ExplainSelf()
+    {
+        return $"I am {this.Name} and my favourite food is {this.FavouriteFood}";
+    }
+}
---
+++ Exercises/06.PolymorphismLab/03.Shapes/Circle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Circle:Shape
+    {
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.Radius = radius;
+    }
+    public double Radius
+    {
+        get
+        {
+            return radius;
+        }
+        set
+        {
+            if (value<=0)
+            {
+                throw new ArgumentException();
+            }
+            radius = value;
+        }
+    }
+
+    public override double CalculateArea()
+    {
+        return 2 * Math.PI * this.Radius;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return Math.PI * this.Radius * this.Radius;
+    }
+
+    public override string Draw()
+    {
+        return base.Draw() + $"{this.GetType().Name}";
+    }
+}
